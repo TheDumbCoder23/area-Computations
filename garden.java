95efc1bedb9083615b4dd98428c8208da49638fe
BlackCoder23 <jpj7176@students.uc-bcf.edu.ph>
2024-09-12 13:32:17 +0800

    Update garden.java

---
+++ garden.java
@@ -0,0 +1,19 @@
+public class garden {
+    public static void main(String[] args) {
+        // My measurements
+        int length = (int) 12; //  length in meters
+        int width = (int) 15;   //  width in meters
+
+        //the area calculation of my garden
+        int area = length * width;
+
+        //the calculation of the perimeters
+        int perimeter = 2 * (length + width);
+
+        //the final result of my gardens measurements
+        System.out.println("Length of the garden: " + length + " meters");
+        System.out.println("Width of the garden: " + width + " meters");
+        System.out.println("Area of the garden: " + area + " square meters");
+        System.out.println("Perimeter of the garden: " + perimeter + " meters");
+    }
+}
